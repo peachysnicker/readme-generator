5693548e88325009a052d299af7baee574fe37be
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-02-08 14:59:05 -0800

    added options to index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
     },
     {
         type: "input",
-        message: "What is the description of the project?",
+        message: "What is the description of your project?",
         name: "description",
     },
     {
@@ -22,17 +22,17 @@ const questions = [
     },
     {
         type: "input",
-        message: "What are the instructions and examples for use?",
+        message: "What are the instructions and examples to use your project?",
         name: "usage",
     },
     {
         type: "list",
         message:
-            "List your collaborators, if any, with links to their GitHub profiles?",
+            "List your collaborators, with links to their GitHub profiles.",
         choices: [
-            "Choice A: https://github.com/coding-boot-camp/potential-enigma",
-            "Choice B: https://www.npmjs.com/package/inquirer/v/8.2.4#questions",
-            "Choice C: No One",
+            "option 1: https://www.google.ca",
+            "option 2: https://www.google.ca",
+            "option 3: None",
         ],
         default: "Choice A: https://github.com/coding-boot-camp/potential-enigma",
         name: "credits",
@@ -48,96 +48,7 @@ const questions = [
             "Choice A: https://www.contributor-covenant.org/version/2/1/code_of_conduct/code_of_conduct.md",
         name: "contribute",
     },
-    {
-        type: "list",
-        message: "What are the tests for this application?",
-        choices: ["Choice A: node test/tests"],
-        default: "test/tests",
-        name: "tests",
-    },
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "github",
-    },
-    {
-        type: "input",
-        message: "What is your GitHub URL link?",
-        name: "githuburl",
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
-    },
-    {
-        type: "list",
-        message: "Choose a license for the application",
-        choices: [
-            "Choice A: Apache License 2.0",
-            "Choice B: MIT License",
-            "Choice C: GNU GPLv2.0",
-        ],
-        default: "MIT License",
-        name: "license",
-    },
-    {
-        type: "list",
-        message: "Status Badge",
-        choices: [
-            "Choice A: Apache License 2.0 ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-            "Choice B: MIT License",
-            "Choice C: GNU GPLv2.0 ![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
-        ],
-        default:
-            "MIT License ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-        name: "badge",
-    },
-];
-
-inquirer
-    .prompt(questions)
-    // TODO: Create a function to write README file
-    .then((response) =>
-        fs.writeFile(
-            "README.md",
-            `# ${response.projecttitle}
-        
-## Description
-${response.description}
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-- [Badges](#badges)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-## Installation <a name="installation"></a>
-${response.installation}
-## Usage <a name="usage"></a>
-${response.usage}
-## Credits <a name="credits"></a>
-${response.credits}
-## Contributing <a name="contributing"></a>
-${response.contribute}
-## Tests <a name="tests"></a>
-${response.tests}
-## License <a name="license"></a>
-${response.license}
-## Badges <a name="badges"></a>
-${response.badge}
-## Questions <a name="questions"></a>
-Contact me with questions at the following links:
-${response.github}
-${response.githuburl}
-${response.email}
-      `,
-            (err) => (err ? console.error(err) : console.log("Success!"))
-        )
-    );
-
+]
 // TODO: Create a function to initialize app
 //function init() { }
 
